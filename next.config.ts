2ae9114a29ddb181118186da1644648e0fd33b92
Jeremiah Ogunleye <jeremiahogunleye604@gmail.com>
2025-09-16 17:30:38 +0100

    next

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config : any, { isServer }:{ isServer: boolean}) => {
-    if (isServer) {
-      config.externals.push({
-        '@prisma/client': 'commonjs @prisma/client',
-      });
-    }
-    
-    // Handle WASM files
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-    };
-
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource',
-    });
-
-    return config;
-  },
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client'],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
