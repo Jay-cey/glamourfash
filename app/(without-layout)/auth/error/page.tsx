8d011fa9a0762e16be6705b5026da63eaabb5d83
Jeremiah <jeremiahogunleye604@gmail.com>
2025-09-16 01:26:43 +0100

    fix param in error page

--- app/(without-layout)/auth/error/page.tsx
+++ app/(without-layout)/auth/error/page.tsx
@@ -1,25 +1,23 @@
-"use client"
-import { useSearchParams } from "next/navigation"
-
 export default function AuthErrorPage({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined }
-}) {
-  const error =
-    typeof searchParams?.error === "string" ? searchParams.error : undefined
+    searchParams,
+    }: {
+      searchParams: { [key: string]: string | string[] | undefined }
+      }) {
+        const error =
+            typeof searchParams?.error === "string" ? searchParams.error : undefined
 
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold mb-4">Authentication Error</h1>
-      {error === "OAuthAccountNotLinked" ? (
-        <p>
-          The email is already linked to another provider. Please sign in using
-          the original provider.
-        </p>
-      ) : (
-        <p>{error || "An unknown error occurred."}</p>
-      )}
-    </div>
-  )
-}
+              return (
+                  <div className="flex flex-col items-center justify-center min-h-screen">
+                        <h1 className="text-2xl font-bold mb-4">Authentication Error</h1>
+                              {error === "OAuthAccountNotLinked" ? (
+                                      <p>
+                                                The email is already linked to another provider. Please sign in using
+                                                          the original provider.
+                                                                  </p>
+                                                                        ) : (
+                                                                                <p>{error || "An unknown error occurred."}</p>
+                                                                                      )}
+                                                                                          </div>
+                                                                                            )
+                                                                                            }
+})
