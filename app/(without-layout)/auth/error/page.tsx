fdbf8d78377bcc481828474b7ae64195225f1942
Jeremiah <jeremiahogunleye604@gmail.com>
2025-09-16 01:18:08 +0100

    fix error.tsx

--- app/(without-layout)/auth/error/page.tsx
+++ app/(without-layout)/auth/error/page.tsx
@@ -1,22 +1,24 @@
+"use client"
+import { useSearchParams } from "next/navigation"
 
 export default function AuthErrorPage({
   searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined }
+  searchParams?: { [key: string]: string | string[] | undefined }
 }) {
-  const error = searchParams.error
+  const error =
+    typeof searchParams?.error === "string" ? searchParams.error : undefined
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-2xl font-bold mb-4">Authentication Error</h1>
       {error === "OAuthAccountNotLinked" ? (
         <p>
-          An account with this email already exists. <br />
-          Please log in with your email & password, then link your GitHub/Google account in
-          settings.
+          The email is already linked to another provider. Please sign in using
+          the original provider.
         </p>
       ) : (
-        <p>Something went wrong. Please try again.</p>
+        <p>{error || "An unknown error occurred."}</p>
       )}
     </div>
   )
