c37371be3afa5297ac4f3d1d021c1f61810ed1c3
Jeremiah <jeremiahogunleye604@gmail.com>
2025-11-04 13:37:18 +0100

    fix

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,13 @@ const nextConfig: CustomNextConfig = {
   serverExternalPackages: ['@prisma/client', 'prisma'],
   webpack: (config: Configuration) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true, layers: true };
+
+    config.module = config.module || {};
+    config.module.rules = config.module.rules || [];
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+    });
     return config;
   },
 }
